c5a5194319ca46e3e8b9fc6c8a595a116a76146e
Daksh Nakra <dnakra@bsd220.org>
2022-10-12 10:05:53 -0500

    Initial Commit

---
+++ Weekly Payroll/Weekly_PayrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weekly_PayrollApp.swift
+//  Weekly Payroll
+//
+//  Created by Daksh Nakra on 9/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Weekly_PayrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
